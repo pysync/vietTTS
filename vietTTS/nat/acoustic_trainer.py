07d8bd0fad7befc4208768a549065254967e0c2a
ntt123 <ntt123@earth>
2021-05-19 19:10:42 +0800

    exponential decay of lr

--- vietTTS/nat/acoustic_trainer.py
+++ vietTTS/nat/acoustic_trainer.py
@@ -47,7 +47,8 @@ loss_vag = jax.value_and_grad(train_loss_fn, has_aux=True)
 
 optimizer = optax.chain(
     optax.clip_by_global_norm(1.0),
-    optax.adam(FLAGS.learning_rate)
+    optax.adam(FLAGS.learning_rate),
+    optax.scale_by_schedule(optax.exponential_decay(1.0, 50_000, 0.2, 50_000))
 )
 
 
