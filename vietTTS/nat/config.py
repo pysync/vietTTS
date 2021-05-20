af57b486a5ca6d63e630ad003b8608e224c75311
ntt123 <ntt123@earth>
2021-05-20 18:17:24 +0800

    back to original model

--- vietTTS/nat/config.py
+++ vietTTS/nat/config.py
@@ -10,7 +10,7 @@ class FLAGS(Namespace):
   vocab_size = 256
   duration_embed_dropout_rate = 0.5
   num_training_steps = 200_000
-  postnet_dim = 256
+  postnet_dim = 512
   acoustic_decoder_dim = 512
   acoustic_encoder_dim = 256
 
